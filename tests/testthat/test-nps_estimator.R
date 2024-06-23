9c427d5076e015ea5ec71e9320a18511202ec6b3
Tamas SmahajcsikSzabo <tamas.smahajcsikszabo@outlook.hu>
2024-06-23 22:17:29 +0200

    format

--- tests/testthat/test-nps_estimator.R
+++ tests/testthat/test-nps_estimator.R
@@ -1,5 +1,6 @@
 test_that("NPS estimator test", {
-  data <- c(10, 7, 6,9,10,8,10,10, 9,8,7,5,8,0,9)
-  expected <- 0.27
-  expect_equal(round(nps_estimator(data),2), expected)
+    data <- c(10, 7, 6, 9, 10, 8, 10, 10, 9, 8, 7, 5, 8, 0, 9)
+    expected <- 0.27
+    expect_equal(round(nps_estimator(data), 2), expected)
 })
+
