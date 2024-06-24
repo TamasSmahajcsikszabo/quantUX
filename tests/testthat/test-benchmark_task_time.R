e63eb8d62a3bb06cb4f8c56ce440e09c381c31ff
Tamas SmahajcsikSzabo <tamas.smahajcsikszabo@outlook.hu>
2024-06-24 21:35:05 +0200

    update

--- tests/testthat/test-benchmark_task_time.R
+++
@@ -1,8 +0,0 @@
-data <- c(215, 131, 260, 171, 187, 147, 74, 170, 131, 165, 347, 90)
-benchmark_value <- 60
-
-test_that("Test task time benchmark", {
-    expected <- 0.9999945
-    expect_equal(round(benchmark_task_time(data, benchmark_value), 7), expected)
-})
-
